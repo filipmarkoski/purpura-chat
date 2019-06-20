9e79cc5d7f20987eee693d688689904b103707b2
Gjorgjina <gjorgjinac@gmail.com>
2019-06-20 20:11:54 +0200

    refactored RegisterActivity

--- app/src/main/java/com/purpura/googlemaps2018/ui/RegisterActivity.java
+++ app/src/main/java/com/purpura/googlemaps2018/ui/RegisterActivity.java
@@ -13,15 +13,13 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.purpura.googlemaps2018.R;
-import com.purpura.googlemaps2018.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.purpura.googlemaps2018.R;
+import com.purpura.googlemaps2018.models.User;
 
 import static android.text.TextUtils.isEmpty;
 import static com.purpura.googlemaps2018.util.Check.doStringsMatch;
@@ -31,94 +29,69 @@ public class RegisterActivity extends AppCompatActivity implements
         View.OnClickListener
 {
     private static final String TAG = "RegisterActivity";
-
-    //widgets
     private EditText mEmail, mPassword, mConfirmPassword, mAge;
     private ProgressBar mProgressBar;
-
-    //vars
     private FirebaseFirestore mDb;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        mEmail = (EditText) findViewById(R.id.input_email);
-        mPassword = (EditText) findViewById(R.id.input_password);
-        mConfirmPassword = (EditText) findViewById(R.id.input_confirm_password);
 
-
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-
+        mEmail = findViewById(R.id.input_email);
+        mPassword = findViewById(R.id.input_password);
+        mConfirmPassword = findViewById(R.id.input_confirm_password);
+        mProgressBar = findViewById(R.id.progressBar);
 
         findViewById(R.id.btn_register).setOnClickListener(this);
-
         mDb = FirebaseFirestore.getInstance();
-
         hideSoftKeyboard();
     }
 
-    /**
-     * Register a new email and password to Firebase Authentication
-     * @param email the email address of the user
-     * @param password the password
-     */
+
     public void registerNewEmail(final String email, String password){
 
         showDialog();
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                .addOnCompleteListener(task -> {
+                    Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
 
-                        if (task.isSuccessful()){
-                            Log.d(TAG, "onComplete: AuthState: " + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete: AuthState: " + FirebaseAuth.getInstance().getCurrentUser().getUid());
 
-                            //insert some default data
-                            User user = new User();
-                            user.setEmail(email);
-                            user.setUsername(email.substring(0, email.indexOf("@")));
-                            user.setUser_id(FirebaseAuth.getInstance().getUid());
+                        User user = new User();
+                        user.setEmail(email);
+                        user.setUser_id(FirebaseAuth.getInstance().getUid());
 
-                            DocumentReference newUserRef = mDb
-                                    .collection(getString(R.string.collection_users))
-                                    .document(FirebaseAuth.getInstance().getUid());
+                        DocumentReference newUserRef = mDb
+                                .collection(getString(R.string.collection_users))
+                                .document(FirebaseAuth.getInstance().getUid());
 
-                            newUserRef.set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    hideDialog();
-
-                                    if(task.isSuccessful()){
-                                        redirectLoginScreen();
-                                    }else{
-                                        View parentLayout = findViewById(android.R.id.content);
-                                        Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
-                                    }
+                        newUserRef.set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                hideDialog();
+                                if (task.isSuccessful()) {
+                                    redirectLoginScreen();
+                                } else {
+                                    View parentLayout = findViewById(android.R.id.content);
+                                    Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
                                 }
-                            });
+                            }
+                        });
 
-                        }
-                        else {
-                            View parentLayout = findViewById(android.R.id.content);
-                            Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
-                            hideDialog();
-                        }
-
-                        // ...
+                    } else {
+                        View parentLayout = findViewById(android.R.id.content);
+                        Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
+                        hideDialog();
                     }
+
                 });
     }
 
-    /**
-     * Redirects the user to the login screen
-     */
-    private void redirectLoginScreen(){
-        Log.d(TAG, "redirectLoginScreen: redirecting to login screen.");
 
+    private void redirectLoginScreen(){
         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
@@ -145,17 +118,13 @@ public class RegisterActivity extends AppCompatActivity implements
             case R.id.btn_register:{
                 Log.d(TAG, "onClick: attempting to register.");
 
-                //check for null valued EditText fields
                 if(!isEmpty(mEmail.getText().toString())
                         && !isEmpty(mPassword.getText().toString())
                         && !isEmpty(mConfirmPassword.getText().toString())){
 
-                    //check if passwords match
                     if(doStringsMatch(mPassword.getText().toString(), mConfirmPassword.getText().toString())){
-
-                        //Initiate registration task
                         registerNewEmail(mEmail.getText().toString(), mPassword.getText().toString());
-                    }else{
+                    } else {
                         Toast.makeText(RegisterActivity.this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
                     }
 
