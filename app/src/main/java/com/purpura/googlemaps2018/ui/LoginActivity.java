a006ec7507caba3c2197ad8d4b368f41ad9708f6
Gjorgjina <gjorgjinac@gmail.com>
2019-06-23 16:08:31 +0200

    added icon

--- app/src/main/java/com/purpura/googlemaps2018/ui/LoginActivity.java
+++ app/src/main/java/com/purpura/googlemaps2018/ui/LoginActivity.java
@@ -2,7 +2,6 @@ package com.purpura.googlemaps2018.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -19,7 +18,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
@@ -132,11 +130,6 @@ public class LoginActivity extends AppCompatActivity implements
                             }
                         });
                     }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    System.out.println("XXX");
                 }
             });
 
