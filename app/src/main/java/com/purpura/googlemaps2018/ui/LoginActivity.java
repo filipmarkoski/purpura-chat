88eceb1aa46dde622c6e34548007f908d5957bce
Душица Јанковиќ <dusica.jankovik@gmail.com>
2019-06-27 22:52:10 +0200

    LogIn Page identical

--- app/src/main/java/com/purpura/googlemaps2018/ui/LoginActivity.java
+++ app/src/main/java/com/purpura/googlemaps2018/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package com.purpura.googlemaps2018.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
@@ -130,6 +132,11 @@ public class LoginActivity extends AppCompatActivity implements
                             }
                         });
                     }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    System.out.println("XXX");
                 }
             });
 
